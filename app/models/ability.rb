90fdb1f921ada01ddc81fe254404bc78654c398c
Anna Romanova <lyashenko.ann@gmail.com>
2015-02-26 17:50:53 -0800

    Added abilities for Users and fixed layouts

---
+++ app/models/ability.rb
@@ -0,0 +1,18 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    unless user
+      can :read, :all
+    else
+      can :read, :all
+      can :manage, Album, user_id: user.id
+      can :new, Album, user.albums.include?(:album_id)
+      can :manage, Photo do |photo|
+        user.albums.include?(photo.album)
+      end
+      can :new, Photo, user.albums.include?(:album_id)
+      can :manage, User, id: user.id
+    end
+  end
+end
