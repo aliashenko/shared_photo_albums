1b9931e5b1712efa6baedac4e657af45c01951a6
Anna Romanova <lyashenko.ann@gmail.com>
2015-03-10 20:32:48 -0700

    Edit static html and css for Album show action

--- app/views/albums/static pages/index.js
+++ app/views/albums/static pages/index.js
@@ -1,3 +1,24 @@
+(function ($) {
+  $.fn.resizeimage = function () {
+    return this.each(function() {
+      var maxHeight = 180;
+      var ratio = 0;
+      var width = $(this).width();
+      var height = $(this).height();
+
+      // Check if current height is larger than max
+      if(height > maxHeight) {
+        ratio = maxHeight / height; // get ratio for scaling image
+        $(this).css("height", maxHeight);
+        $(this).css("width", width * ratio);
+        width = width * ratio;
+        height = height * ratio;
+      };
+    });
+  }
+})(jQuery);
+
 $(document).ready(function() {
   $(".dropdown-button").dropdown({hover: false, alignment: 'right', constrain_width: false});
+  $('.slider').resizeimage();
 });
