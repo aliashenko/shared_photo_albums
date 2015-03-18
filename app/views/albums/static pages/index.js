0618a3bc748f4bf3980692124c69793c769cb2c9
Anna Romanova <lyashenko.ann@gmail.com>
2015-03-17 17:33:01 -0700

    Resize images for FullScreen view

--- app/views/albums/static pages/index.js
+++ app/views/albums/static pages/index.js
@@ -1,24 +1,63 @@
 (function ($) {
-  $.fn.resizeimage = function () {
-    return this.each(function() {
-      var maxHeight = 180;
-      var ratio = 0;
-      var width = $(this).width();
-      var height = $(this).height();
+  $.fn.resizeimage = function (options) {
+    var defaults = {
+      maxHeight: 180,
+      minimize: true,
+      ratio: 0
+    }
 
-      // Check if current height is larger than max
-      if(height > maxHeight) {
-        ratio = maxHeight / height; // get ratio for scaling image
-        $(this).css("height", maxHeight);
-        $(this).css("width", width * ratio);
-        width = width * ratio;
-        height = height * ratio;
+    options = $.extend(defaults, options);
+    return this.each(function(){
+      var origin = $(this);
+
+      function updateOptions() {
+        if (origin.data('maxHeight') != undefined)
+          options.maxHeight = origin.data('maxHeight');
+        if (origin.data('minimize') != undefined)
+          options.minimize = origin.data('minimize');
+        if (origin.data('ratio') != undefined)
+          options.ratio = origin.data('ratio');
+      }
+
+      updateOptions();
+
+      var originalWidth = origin.width();
+      var originalHeight = origin.height();
+      var windowWidth = window.innerWidth;
+      var windowHeight = window.innerHeight;
+
+      var widthPercent = originalWidth / windowWidth;
+      var heightPercent = originalHeight / windowHeight;
+
+      var newWidth = 0;
+      var newHeight = 0;
+      var ratio = 0;
+
+      if(options.minimize) {
+        ratio = options.maxHeight / originalHeight; // get ratio for scaling image
+        newWidth = originalWidth * ratio;
+        newHeight = originalHeight * ratio;
+      }
+      else {
+        if (widthPercent > heightPercent) {
+          ratio = originalHeight / originalWidth;
+          newWidth = windowWidth * 0.9;
+          newHeight = windowWidth * 0.9 * ratio;
+        }
+        else {
+          ratio = originalWidth / originalHeight;
+          newWidth = (windowHeight * 0.9) * ratio;
+          newHeight = windowHeight * 0.9;
+        }
       };
+      origin.css("height", newHeight);
+      origin.css("width", newWidth);
     });
-  }
+  };
 })(jQuery);
 
 $(document).ready(function() {
   $(".dropdown-button").dropdown({hover: false, alignment: 'right', constrain_width: false});
   $('.slider').resizeimage();
+  $('.fullsize').resizeimage({minimize: false});
 });
