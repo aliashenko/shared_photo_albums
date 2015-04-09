142b12fa653cd80a6b68198ecd0977116f0471fe
Anna Romanova <lyashenko.ann@gmail.com>
2015-04-08 22:40:21 -0700

    Add kaminari gem, custom pagination views and User pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show]
 
   def index
-    @users = User.all
+    @users = User.all.order(:name).page(params[:page]).per(10)
   end
 
   def show
