176a0ac3d2358bb5e7da4229ebbe972948ef9938
Anna Romanova <lyashenko.ann@gmail.com>
2015-04-09 22:26:06 -0700

    Setup mailcatcher smtp server for sending emails

--- app/mailers/share_album_mailer.rb
+++ app/mailers/share_album_mailer.rb
@@ -4,7 +4,7 @@ class ShareAlbumMailer < ActionMailer::Base
   def welcome_email(email, album)
     @email = email
     @album = album
-    @url = 'http://sharemoments.com/login'
-    mail(to: @email, subject: 'Welcome to My Awesome Site')
+    @url = 'http://sharemoments.com/user/sign_in'
+    mail(to: @email, subject: 'Welcome to ShareMoments.com')
   end
 end
