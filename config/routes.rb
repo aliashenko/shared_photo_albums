4795ee9edf376b2bd37a6c7b88bc55c818bf3b56
Anna Romanova <lyashenko.ann@gmail.com>
2015-04-24 20:53:59 -0700

    Edit Sign in and Login layout

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,15 @@ Rails.application.routes.draw do
   root to: 'users#index'
 
   devise_for :user,
-             :controllers => { :omniauth_callbacks => 'omniauth_callbacks',
-                               :registrations => 'users/registrations' }
+             :controllers => { omniauth_callbacks: 'omniauth_callbacks',
+                               registrations: 'users/registrations' },
+             :skip => [:sessions]
+
+  as :user do
+    get 'login' => 'devise/sessions#new', as: :new_user_session
+    post 'login' => 'devise/sessions#create', as: :user_session
+    delete 'logout' => 'devise/sessions#destroy', as: :destroy_user_session
+  end
 
   resources :users, only: [:index, :show]
   resources :albums do
